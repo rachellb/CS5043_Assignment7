cb38154298386c3d02a88ae77b783150d5498a8f
rachellb <rachel.l.bennett-1@ou.edu>
2022-04-27 13:05:06 -0500

    updated batch file

--- batch.sh
+++ batch.sh
@@ -5,8 +5,8 @@
 #SBATCH --cpus-per-task=10
 # memory in MB
 #SBATCH --mem=2048
-#SBATCH --output=results/molecule_%04a_stdout.txt
-#SBATCH --error=results/molecule_%04a_stderr.txt
+#SBATCH --output=results/molecule_1_%04a_stdout.txt
+#SBATCH --error=results/molecule_1_%04a_stderr.txt
 #SBATCH --time=24:00:00
 #SBATCH --job-name=molecule_1_exp
 #SBATCH --mail-user=rachel.l.bennett-1@ou.edu
@@ -17,5 +17,5 @@
 . /home/fagg/tf_setup.sh
 conda activate tf
 
-python hw7_base.py @parameters.txt --attention 10 10 --exp_index $SLURM_ARRAY_TASK_ID
+python hw7_base.py @parameters.txt --label "round2" --attention 5 5 5 5 --exp_index $SLURM_ARRAY_TASK_ID
 
